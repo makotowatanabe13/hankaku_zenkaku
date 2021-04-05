9cd697e750dc4ee2ca2c1b3a93221f93a255036e
makwa <makotowatanabe13@gmail.com>
2021-04-05 11:02:22 +0900

    add non ascii

--- ConsoleAppTest/Program.cs
+++ ConsoleAppTest/Program.cs
@@ -7,15 +7,24 @@ namespace ConsoleAppTest
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-            string temp;
-            temp = "ｱ ｲ ｳ ｴ ｵ ｶ ｷ ｸ ｹ ｺ ｻ ｼ ｽ ｾ ｿ ﾀ ﾁ ﾂ ﾃ ﾄ ﾅ ﾆ ﾇ ﾈ ﾉ ﾊ ﾋ ﾌ ﾍ ﾎ ﾏ ﾐ ﾑ ﾒ ﾓ ﾔ ﾕ ﾖ ﾗ ﾘ ﾙ ﾚ ﾛ ﾜ ｦ ﾝ"+
+            Convert(
+            "ｱ ｲ ｳ ｴ ｵ ｶ ｷ ｸ ｹ ｺ ｻ ｼ ｽ ｾ ｿ ﾀ ﾁ ﾂ ﾃ ﾄ ﾅ ﾆ ﾇ ﾈ ﾉ ﾊ ﾋ ﾌ ﾍ ﾎ ﾏ ﾐ ﾑ ﾒ ﾓ ﾔ ﾕ ﾖ ﾗ ﾘ ﾙ ﾚ ﾛ ﾜ ｦ ﾝ"+
 
 "ｳﾞ ｶﾞ ｷﾞ ｸﾞ ｹﾞ ｺﾞ ｻﾞ ｼﾞ ｽﾞ ｾﾞ ｿﾞ ﾀﾞ ﾁﾞ ﾂﾞ ﾃﾞ ﾄﾞ ﾊﾞ ﾋﾞ ﾌﾞ ﾍﾞ ﾎﾞ ﾊﾟ ﾋﾟ ﾌﾟ ﾍﾟ ﾎﾟ ｧ ｨ ｩ ｪ ｫ ｬ ｭ ｮ ｯ"+
 
-"ｰ ﾞ ﾟ ､ ｡ ･ ｢ ｣";
+"ｰ ﾞ ﾟ ､ ｡ ･ ｢ ｣");
+            
+
+            Convert( "⦅ ⦆ ¢ £ ¬ ¯ ¦ ¥ ₩ ￨ ￩ ￪ ￫ ￬ ￭ ￮");
+
+        }
+        static void Convert(string temp)
+        {
             Console.WriteLine($"From =={temp}");
             temp = HankakuUtil.ConsolidateTextToFullWidth(temp);
             Console.WriteLine($"To =={temp}");
         }
+
+        //  https://so-zou.jp/web-app/text/fullwidth-halfwidth/
     }
 }
